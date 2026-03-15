cab44a3331d2f6beb2c55dfdf3d57364dccf284c
farhan-ahmed246 <fmukhtar420@gmail.com>
2026-03-15 13:46:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -776,7 +776,5 @@ body{background:black;color:white;font-family:sans-serif;}
 
 
 
-# ================= APP RUN =================
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
